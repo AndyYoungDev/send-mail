d5f9886a3293f809e0c2f588e0e198a2a72c4313
yanglongfei <283598349@qq.com>
2019-02-19 15:14:16 +0800

    备份提交

---
+++ Form1.Designer.cs
@@ -0,0 +1,154 @@
+﻿namespace 群发
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.id = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.name = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.email = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.status = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.created_at = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.id,
+            this.name,
+            this.email,
+            this.status,
+            this.created_at});
+            this.dataGridView1.Location = new System.Drawing.Point(12, 58);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.RowTemplate.Height = 23;
+            this.dataGridView1.Size = new System.Drawing.Size(734, 393);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // id
+            // 
+            this.id.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.id.HeaderText = "编号";
+            this.id.Name = "id";
+            this.id.ReadOnly = true;
+            // 
+            // name
+            // 
+            this.name.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.name.HeaderText = "名称";
+            this.name.Name = "name";
+            this.name.ReadOnly = true;
+            // 
+            // email
+            // 
+            this.email.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.email.HeaderText = "邮箱";
+            this.email.Name = "email";
+            this.email.ReadOnly = true;
+            // 
+            // status
+            // 
+            this.status.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.status.HeaderText = "状态";
+            this.status.Name = "status";
+            this.status.ReadOnly = true;
+            // 
+            // created_at
+            // 
+            this.created_at.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.created_at.HeaderText = "发件时间";
+            this.created_at.Name = "created_at";
+            this.created_at.ReadOnly = true;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Tomato;
+            this.button1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(100, 40);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "导入数据";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Tomato;
+            this.button2.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button2.Location = new System.Drawing.Point(127, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(100, 40);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "开始发送";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(758, 457);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dataGridView1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "群发";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn id;
+        private System.Windows.Forms.DataGridViewTextBoxColumn name;
+        private System.Windows.Forms.DataGridViewTextBoxColumn email;
+        private System.Windows.Forms.DataGridViewTextBoxColumn status;
+        private System.Windows.Forms.DataGridViewTextBoxColumn created_at;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace 群发
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            System.Windows.Forms.Control.CheckForIllegalCrossThreadCalls = false;
+
+            InitializeComponent();
+
+        }
+        private void sendMail()
+        {
+            string localPath = System.IO.Directory.GetCurrentDirectory();
+            StreamReader sr = new StreamReader(localPath+"/发件html", System.Text.Encoding.UTF8);
+            string restOfStream = sr.ReadToEnd();
+            for (int i = 0; i < dataGridView1.RowCount; i++)
+            {
+                try
+                {
+                    MailMessage message = new MailMessage();
+                    MailAddress fromAddr = new MailAddress("邮箱");
+                    message.From = fromAddr;
+                    message.To.Add(dataGridView1.Rows[i].Cells[2].Value.ToString());
+                    message.Subject = dataGridView1.Rows[i].Cells[1].Value.ToString();
+                    message.IsBodyHtml = true;
+
+                    message.Attachments.Add(new Attachment(localPath + "/a.png"));
+                    message.Attachments[0].ContentType.Name = "image/gif";
+                    message.Attachments[0].ContentId = "a";
+                    message.Attachments[0].ContentDisposition.Inline = true;
+                    message.Attachments[0].TransferEncoding = System.Net.Mime.TransferEncoding.Base64;
+
+                    //<img src="cid:a">
+
+
+
+                    message.Body = restOfStream;
+
+
+                    message.Attachments.Add(new Attachment("文件"));
+                    SmtpClient client = new SmtpClient("smtp地址", 25);
+                    client.EnableSsl = false;
+                    client.UseDefaultCredentials = false;
+                    client.Credentials = new NetworkCredential("邮箱", "密码");
+
+                    client.DeliveryMethod = SmtpDeliveryMethod.Network;
+                    client.Send(message);
+                    dataGridView1.Rows[i].Cells[3].Value = "已发送";
+                    dataGridView1.Rows[i].Cells[4].Value = DateTime.Now.ToString(); ;
+                }
+                catch (Exception ee)
+                {
+                    dataGridView1.Rows[i].Cells[3].Value = "发送失败";
+                }
+            }
+            button2.Enabled = true;
+            button1.Enabled = true;
+            button2.Text = "开始发送";
+        }
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            OpenFileDialog pOpenFileDialog = new OpenFileDialog();
+            pOpenFileDialog.Filter = "所有文件|*.txt";
+            pOpenFileDialog.Multiselect = false;
+            pOpenFileDialog.Title = "打开文件";
+            if (pOpenFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                string path = pOpenFileDialog.FileName;
+                this.readFile(path);
+            }
+        }
+        private void readFile(string filename)
+        {
+            StreamReader sr = new StreamReader(filename, Encoding.Default);
+            String line;
+            ArrayList data = new ArrayList();
+            while ((line = sr.ReadLine()) != null)
+            {
+                data.Add(line);
+            }
+            for (int i = 0; i < data.Count; i++)
+            {
+                string[] sArray = Regex.Split(data[i].ToString(), "----", RegexOptions.IgnoreCase);
+
+                if (sArray[0] !=""&& sArray[1]!="") {
+                    int index = this.dataGridView1.Rows.Add();
+                    this.dataGridView1.Rows[index].Cells[0].Value = i + 1;
+                    this.dataGridView1.Rows[index].Cells[1].Value = sArray[0];
+                    this.dataGridView1.Rows[index].Cells[2].Value = sArray[1];
+                    this.dataGridView1.Rows[index].Cells[3].Value = "未发送";
+                    this.dataGridView1.Rows[index].Cells[4].Value = "无";
+                }
+
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            button2.Text = "正在发送";
+            button2.Enabled = false;
+            button1.Enabled = false;
+            Task.Factory.StartNew(sendMail);
+        }
+    }
+}
